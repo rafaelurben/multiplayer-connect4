a88cc5ea44fb9d86515d6e4d963c58e0dc2ac6e8
Rafael Urben <rafael.urben@ict-campus.net>
2023-10-25 15:39:43 +0200

    Prepared game and player model

--- connect4server/game.py
+++ connect4server/game.py
@@ -1,12 +1,24 @@
 "Game class"
 
-class Game:
-    games = {}
-    lastgameid = 0
+import typing
 
-    def __init__(self):
-        self.lastgameid += 1
-        self.gameid = self.lastgameid
-        self.games[self.gameid] = self
+class Game:
+    games : typing.Dict[int, "Game"] = {}
+    lastgameid : int = 0
+
+    def __init__(self, p1id, p2id):
+        Game.lastgameid += 1
+        self.gameid : int = Game.lastgameid
+        Game.games[self.gameid] = self
+
+        self.p1id = p1id
+        self.p2id = p2id
     
+    @property
+    def id(self):
+        return self.gameid
+
+    def delete(self):
+        del Game.games[self.gameid]
+
     # TODO: Create game model
---
+++ connect4server/player.py
@@ -0,0 +1,46 @@
+"Player model"
+
+import typing
+import re
+
+class Player:
+    everyone : typing.Dict[int, "Player"] = {}
+
+    def __init__(self, name : str, plid : int) -> None:
+        self.name : str = name
+        self.playerid : int = plid
+        self.gameid : int = None
+
+        Player.everyone[self.playerid] = self
+
+    @property
+    def id(self):
+        return self.playerid
+
+    def is_in_game(self) -> bool:
+        return self.gameid is not None
+
+    def as_dict(self) -> dict:
+        return {
+            "name": self.name,
+            "id": self.playerid,
+            "gameid": self.gameid,
+        }
+
+    def delete(self):
+        del Player.everyone[self.playerid]
+
+    @classmethod
+    def name_check(self, name: str) -> bool:
+        "Check if a name is valid"
+
+        # Check for duplicate names
+        for player in Player.everyone.values():
+            if player.name == name:
+                return False
+
+        # Validate name
+        if not re.match(r'^[a-zA-Z0-9_\- ]{3,20}$', name):
+            return False
+
+        return True
